d53af7d7eef582ae869a66b3d39c3e60ea397ed9
ronchitech <ronchibusiness@gmail.com>
2021-07-10 16:13:16 +0800

    REST APIs, Clients & CORS errors

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = express();
 
 // app.use(bodyParser.urlencoded({ extended: false })); //x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+});
+
 app.use(feedRoutes);
 
 app.use('/', (req, res, next) => {

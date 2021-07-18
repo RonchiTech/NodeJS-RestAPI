7964c7524d03d52ab0057c450d15543058082972
ronchitech <ronchibusiness@gmail.com>
2021-07-18 15:10:33 +0800

    sharing the io instance across files

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ mongoose
       console.log('Server is running on port 8080');
     });
     // console.log('SERVER:',server);
-    const io = require('socket.io')(server, {
+    const io = require('./socket').init(server, {
       cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
